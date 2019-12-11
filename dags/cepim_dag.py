1101d1a85cc1c873fb5a565ad851e92201318e3d
chris-redfield <christiamoryah@gmail.com>
2019-12-11 11:08:34 -0300

    fix dag - 100%

--- dags/cepim_dag.py
+++ dags/cepim_dag.py
@@ -5,7 +5,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from selenium_scripts.cepim import get_url
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import logging
 import pandas as pd
 
@@ -27,7 +27,8 @@ class ExtendedPythonOperator(PythonOperator):
 args = {
     'owner': 'COGIT-ME',
     'depends_on_past': False,
-    'start_date': datetime(2019, 12, 1),
+    'start_date': datetime(2019, 12, 10),
+    #'start_date': datetime(date.today().year, date.today().month, date.today().day),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -79,11 +80,20 @@ def get_data_from_url (target_url, target_file):
 # ----------------------------- TAREFA 3 -----------------------------------------------------------------------
 
 def unzip_file (from_file, to_file):
+
+    print("================================================================")
+    print("================================================================")
+    print("================================================================")
+
+    
+
     with zipfile.ZipFile(from_file, 'r') as zip_ref:
-            zip_ref.extractall()
+            #zip_ref.extractall()
+            zip_ref.extractall(path=local_downloads)
             with open(to_file, 'w') as f:
                 f.write(zip_ref.namelist()[0])
-                f.write("\n")
+                print("unzipped file:",zip_ref.namelist()[0], "to", local_downloads)
+                #f.write("\n")
 
 
 # --------------------------------------------------------------------------------------------------------------
@@ -91,22 +101,34 @@ def unzip_file (from_file, to_file):
 
 def copy_to_sqlserver (nome_tabela, nome_arquivo):
 
-    #Parametros da conexão (conectando com o banco no latitude)
-    server = 'HOST,PORT'
-    database = 'CEPIM'
-    username = 'USR'
-    password = 'PASS'
+    print("================================================================")
+    print("================================================================")
+    print("================================================================")
 
-    #
+    #Parametros da conexão (conectando com o banco no latitude)
+    server = f"{os.environ['LAKE_HOST']},{os.environ['LAKE_PORT']}"
+    database = os.environ['CEPIM_DATABASE']
+    username = os.environ['LAKE_USER']
+    password = os.environ['LAKE_PASS']
+
+    print('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     #Fazendo a conexão
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
-    #
+
     #ESVAZIA A TABELA PARA SER ATUALIZADA
     deleta = "DELETE " + nome_tabela
     cursor.execute(deleta) 
 
-    panda = pd.read_csv(nome_arquivo,sep=';',encoding='latin')
+    with open(nome_arquivo) as file_csv_name:
+        csv_name = file_csv_name.readline()
+    file_csv_name.close()
+
+    csv_name = os.path.join(local_downloads,csv_name)
+
+    print("CSV FILE TO OPEN:", csv_name)
+
+    panda = pd.read_csv(csv_name,sep=';',encoding='latin')
     new_columns = []
     for value in panda.columns.values:
         new_columns.append(remove_accents(value.replace(' ','_')))
@@ -124,14 +146,15 @@ def copy_to_sqlserver (nome_tabela, nome_arquivo):
 # ----------------------------- TAREFA 5,6,7,8 -----------------------------------------------------------------
 
 def delete_file (file_name):
-    if os.path.exists(file_name):
-        os.remove(file_name)
+   if os.path.exists(file_name):
+       os.remove(file_name)
 
 def delete_from_file (file_name):
-    with open(file_name, 'r', encoding="utf-8") as f:
-        file_delete = f.readlines()
-        delete_file(file_delete[0])
-        f.close()
+   with open(file_name, 'r', encoding="utf-8") as f:
+       file_delete = f.readlines()
+       file_delete = os.path.join(local_downloads,file_delete[0])
+       delete_file(file_delete)
+       f.close()
 
 #processo: CEPIM
 dag = DAG(dag_id='cepim', default_args=args, schedule_interval=timedelta(days=1))
@@ -163,28 +186,28 @@ t4 = PythonOperator(
     dag=dag)
 
 t5 = PythonOperator(
-    task_id='delete_url_txt',
-    python_callable=delete_file,
-    op_kwargs={'file_name': os.path.join(local_downloads,'cepim_url.txt')},
-    dag=dag)
+   task_id='delete_url_txt',
+   python_callable=delete_file,
+   op_kwargs={'file_name': os.path.join(local_downloads,'cepim_url.txt')},
+   dag=dag)
 
 t6 = PythonOperator(
-    task_id='delete_zip',
-    python_callable=delete_file,
-    op_kwargs={'file_name':os.path.join(local_downloads,'cepim.zip')},
-    dag=dag)
+   task_id='delete_zip',
+   python_callable=delete_file,
+   op_kwargs={'file_name':os.path.join(local_downloads,'cepim.zip')},
+   dag=dag)
 
 t7 = PythonOperator(
-    task_id='delete_csv',
-    python_callable=delete_from_file,
-    op_kwargs={'file_name':os.path.join(local_downloads,'cepim_filename.txt')},
-    dag=dag)
+   task_id='delete_csv',
+   python_callable=delete_from_file,
+   op_kwargs={'file_name':os.path.join(local_downloads,'cepim_filename.txt')},
+   dag=dag)
 
 t8 = PythonOperator(
-    task_id='delete_filename_txt',
-    python_callable=delete_file,
-    op_kwargs={'file_name':os.path.join(local_downloads,'cepim_filename.txt')},
-    dag=dag)
+   task_id='delete_filename_txt',
+   python_callable=delete_file,
+   op_kwargs={'file_name':os.path.join(local_downloads,'cepim_filename.txt')},
+   dag=dag)
 
 # Ordem das operações
 t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7 >> t8
