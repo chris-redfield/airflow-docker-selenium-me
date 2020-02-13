15c40af86e78afc91089b14d429c736da2364926
chris-redfield <christiamoryah@gmail.com>
2020-02-13 20:56:30 -0300

    UPDATE start date / rename sample dag

--- dags/inidoneos_dag.py
+++ dags/inidoneos_dag.py
@@ -15,7 +15,7 @@ import os                                                           # Tarefa 5
 args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 2, 12),
+    'start_date': datetime(2020, 2, 14),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
