410b6da4913e4908e454de715df52b88afdb9b2d
chris-redfield <christiamoryah@gmail.com>
2020-03-25 09:05:07 -0300

    update sancoesSP dag

--- dags/sancoesSP_dag.py
+++ dags/sancoesSP_dag.py
@@ -12,7 +12,8 @@ import time
 import pyodbc
 import pandas as pd
 import csv
-import shutil
+
+import glob
 
 class ExtendedPythonOperator(PythonOperator):
     '''
@@ -24,7 +25,7 @@ class ExtendedPythonOperator(PythonOperator):
 args = {
     'owner': 'COGIT-ME',
     'depends_on_past': False,
-    'start_date': datetime(2020, 3, 23),
+    'start_date': datetime(2020, 3, 25),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -40,21 +41,38 @@ local_downloads = os.path.join(os.getcwd(), 'downloads')
 local_csv = os.path.join(local_downloads,'csv')
 local_xls = os.path.join(local_downloads,'xls')
 
-dict_BEC = {}
-dict_BEC['sancoes'] = {'tipo' : 'SançõesRestritivas', 'id_tipo' : "__tab_ctl00_ContentPlaceHolder1_TabsSancaos_TabSancaoIncluidas", 'cab' : ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO','PERIODO DE SANCAO', 'DATA INICIO', 'DATA TERMINO', 'ABRANGENCIA DA PENALIDADE']}
-dict_BEC['multas'] = {'tipo' : 'Multas', 'id_tipo' : "__tab_ctl00_ContentPlaceHolder1_TabsSancaos_TabMultas", 'cab' : ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO','VALOR MULTA']}
-dict_BEC['advertencias'] = {'tipo' : 'Advertencias', 'id_tipo' : "__tab_ctl00_ContentPlaceHolder1_TabsSancaos_TabAdvertencias", 'cab' : ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO']}
+
+# Aqui as informações pertinentes a cada uma das três bases para poder operá-las em sequencia
+# Procuramos abstrair para aqui dentro o que difere as três bases:
+#   -> Nome
+#   -> ID da função javascript pra acessar as mesmas
+#   -> Dados, representados aqui pelos cabeçalhos
+dicionario_bases = {
+    'sancoes' : {
+        'tipo' : 'SançõesRestritivas',
+        'id_tipo' : "__tab_ctl00_ContentPlaceHolder1_TabsSancaos_TabSancaoIncluidas",
+        'cab' : ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO','PERIODO DE SANCAO', 'DATA INICIO', 'DATA TERMINO', 'ABRANGENCIA DA PENALIDADE']
+    },
+    'multas' : {
+        'tipo' : 'Multas',
+        'id_tipo' : "__tab_ctl00_ContentPlaceHolder1_TabsSancaos_TabMultas",
+        'cab' : ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO','VALOR MULTA']
+    },
+    'advertencias' : {
+        'tipo' : 'Advertencias',
+        'id_tipo' : "__tab_ctl00_ContentPlaceHolder1_TabsSancaos_TabAdvertencias",
+        'cab' : ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO']
+    }
+}
 
 def remove_files():
-    try:
-        for filename in os.listdir(local_xls):
-            os.remove(filename)
-        for filename in os.listdir(local_csv):
-            os.remove(filename)
-        os.rmdir(local_xls)
-        os.rmdir(local_csv)
-    except:
-        exit()
+    for filename in os.listdir(local_xls):
+        os.remove(local_xls + "/" + filename)
+    for filename in os.listdir(local_csv):
+        os.remove(local_csv + "/" + filename)
+    os.rmdir(local_xls)
+    os.rmdir(local_csv)
+
 
 def copy_to_sql_server ():
     nome_tabela = 'Inadimplentes.dbo.Dados_inadimplentes'
@@ -64,7 +82,6 @@ def copy_to_sql_server ():
     username = os.environ['LAKE_USER']
     password = os.environ['LAKE_PASS']
 
-    #
     #Fazendo a conexão
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
@@ -74,10 +91,10 @@ def copy_to_sql_server ():
     cursor.execute(deleta) 
 
     for nome in ['sancoes','multas','advertencias']:
-        nome_arquivo = local_csv+'/output'+dict_BEC[nome]['tipo']+'_normalizado.csv'
+        nome_arquivo = local_csv+'/output'+dicionario_bases[nome]['tipo']+'_normalizado.csv'
         #
         # Preparando os comandos
-        panda = pd.read_csv(nome_arquivo,encoding='latin')
+        panda = pd.read_csv(nome_arquivo,encoding='utf-8')
 
         #TODO row['VALOR MULTA'] retornando nan e dando erro no banco
 
@@ -120,96 +137,72 @@ def copy_to_sql_server ():
         
 
 def normalize_data():
-    try:
-        for filename in os.listdir(local_downloads+"/csv"):
-            return
-    except:
+    for nome in ['sancoes','multas','advertencias']:
+        cab_banco = ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO','PERIODO DE SANCAO','DATA INICIO', 'DATA TERMINO', 'ABRANGENCIA DA PENALIDADE','VALOR MULTA']
 
-        for nome in ['sancoes','multas','advertencias']:
-            cab_banco = ['TIPO_MEDIDA','PODER','SECRETARIA/ORGAO','UGE','NUMERO DO PROCESSO','TIPO DE PESSOA','RAZAO SOCIAL/NOME','CNPJ/CPF','TIPO DE SANCAO','PERIODO DE SANCAO','DATA INICIO', 'DATA TERMINO', 'ABRANGENCIA DA PENALIDADE','VALOR MULTA']
+        filename = open(local_csv+'/output'+dicionario_bases[nome]['tipo']+'.csv', 'r')
+        base_normalizada = [cab_banco]
+        cab_arq = filename.readline().replace('\n','').split(',')
+        for line in filename:
+            line = line.replace('\n','').split(',')
+            reg_final = []
+            for campo in cab_banco:
+                if campo in cab_arq:
+                    reg_final.append(line[cab_arq.index(campo)])
+                else:
+                    reg_final.append('')
+            base_normalizada.append(reg_final)
 
-            filename = open(local_csv+'/output'+dict_BEC[nome]['tipo']+'.csv', 'r')
-            base_normalizada = [cab_banco]
-            cab_arq = filename.readline().replace('\n','').split(',')
-            for line in filename:
-                line = line.replace('\n','').split(',')
-                reg_final = []
-                for campo in cab_banco:
-                    if campo in cab_arq:
-                        reg_final.append(line[cab_arq.index(campo)])
-                    else:
-                        reg_final.append('')
-                base_normalizada.append(reg_final)
-
-            with open(local_csv+'/output'+dict_BEC[nome]['tipo']+'_normalizado.csv', 'w') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerows(base_normalizada)
+        with open(local_csv+'/output'+dicionario_bases[nome]['tipo']+'_normalizado.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(base_normalizada)
 
 def html_to_csv():
-    try:
-        for filename in os.listdir(local_downloads+"/csv"):
-            return
-    except:
-        try:
-            for filename in os.listdir(local_xls):
-                break
-        except:
-            os.mkdir(local_xls)
 
-        try:
-            for filename in os.listdir(local_csv):
-                break
-        except:
-            os.mkdir(local_csv)
-
-        for nome in ['sancoes','multas','advertencias']:
-            try:
-                # O Firefox nao estava atendendo a propriedade de baixar na pasta especificada em codigo. Entao esse trecho faz um move de todos os arquivos de multas na pasta Downloads e leva pra pasta do executavel
-                #dir = '~/Downloads'                               # Para uso em ambientes Linux
-                lista_sancoes = os.listdir(local_downloads)
-                for file in lista_sancoes:
-                    if dict_BEC[nome]['tipo'] in file : shutil.move(local_downloads+'/'+file, local_xls)
-            except Exception as ex :
-                template = "Uma excecao do tipo {0} aconteceu. Argumentos:\n{1!r}"
-                message = template.format(type(ex).__name__, ex.args)
-                print (message)
-                print ('Pasta de download dos arquivos da BEC nao configurada. Saindo...')
-                exit()
+    # Garante que a pasta de csv existe
+    if not os.path.exists(local_csv):
+        os.makedirs(local_csv)
 
 
-            print ('Inicio da leitura: ', time.ctime())
-            full_base = []
-            full_base.append(dict_BEC[nome]['cab'])
-            for filename in Path(local_xls).rglob(dict_BEC[nome]['tipo']+'*'):
-                html = open(filename).read()
-                soup = BeautifulSoup(html,"html.parser")
-                table = soup.find("table")
+    # Opera uma base de cada vez (sancoes, multas e advertencias)
+    for (nome_base, base) in dicionario_bases.items():
 
-                for table_row in table.findAll('tr'):
-                    columns = table_row.findAll('td')
-                    output_row = []
-                    for column in columns[1:]:
-                        text = column.text
-                        text = text.replace('Visualizar descrição do valor','').replace(',','.').replace('\n','').strip()
-                        output_row.append(text)
+        print (f'Inicio da leitura de {nome_base}')
 
-                if output_row != [] and output_row != ['Nenhum registro encontrado']:
-                    full_base.append([dict_BEC[nome]['tipo']] + output_row)
+        full_base = []
+        full_base.append(base['cab'])
 
-            print ('Final da leitura: ', time.ctime())
+        # Para cada arquivo do tipo relevante, ou seja, <pasta xls>/<tipo base>_<pagina>_1<fornecedor>.xls
+        for filename in glob.glob( f"{local_xls}/{base['tipo']}_*"):
+            html = open(filename).read()
+            soup = BeautifulSoup(html,"html.parser")
+            table = soup.find("table")
 
-            with open(local_csv+'/output'+dict_BEC[nome]['tipo']+'.csv', 'w') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerows(full_base)
+            for table_row in table.findAll('tr'):
+                columns = table_row.findAll('td')
+                output_row = []
+                for column in columns[1:]:
+                    text = column.text
+                    text = text.replace('Visualizar descrição do valor','').replace(',','.').replace('\n','').strip()
+                    output_row.append(text)
+
+            if output_row != [] and output_row != ['Nenhum registro encontrado']:
+                full_base.append([base['tipo']] + output_row)
+
+        print ('Final da leitura: ', time.ctime())
+
+        with open(local_csv+'/output'+base['tipo']+'.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(full_base)
 
 # Processo: DADOS_BEC_SP
+
 dag = DAG(dag_id='sancoes_sp', default_args=args, schedule_interval=timedelta(days=30))
-url_request = 'https://www.bec.sp.gov.br/Sancoes_ui/aspx/ConsultaAdministrativaFornecedor.aspx'
 
 t1 = SeleniumOperator(
     task_id='get_url',
     script=get_url,
-    script_args=[local_downloads,dict_BEC,url_request],
+    script_args=[local_downloads, local_xls, dicionario_bases],
     dag=dag)
 
 t2 = PythonOperator(
@@ -236,5 +229,6 @@ t5 = PythonOperator(
     op_kwargs={},
     dag=dag)
 
-# Ordem das operações
+# # Ordem das operações
 t1 >> t2 >> t3 >> t4 >> t5
+
